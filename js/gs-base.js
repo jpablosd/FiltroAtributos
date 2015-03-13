46ff6da9c1f1a8f688a742228a2385a554249c91
jpablosd <juanpablo.soto17@gmail.com>
2015-03-13 18:57:38 -0300

    0.6
    Se agrega el botón con las flechas abajo, el botón cargar valores (la
    nube al lado de valores) que detecta si uno lo aprieta y no esta
    seleccionado un atributo, arroja un alert.

--- js/gs-base.js
+++ js/gs-base.js
@@ -23,127 +23,6 @@ function mostrarFiltros(){
     cargarOperadores();
 }
 
-    
-
-//console.log("valores de "+atributoSeleccionado);
-//    var xmlhttp = new XMLHttpRequest();
-//    var url = "require/cargarFiltro.php?atributoSeleccionado="+atributoSeleccionado+"&operadorSeleccionado="+operadorSeleccionado+"&valorSeleccionado="+valorSeleccionado;
-//    xmlhttp.onreadystatechange=function() {
-//        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//            myFunction(xmlhttp.responseText);
-//        }
-//    }
-//    xmlhttp.open("GET", url, true);
-//    xmlhttp.send();
-//    function myFunction(response) {
-//        var arr = JSON.parse(response);
-//        //var id          = arr[0].id;
-//        var out = "";
-//        //console.log(arr.length);
-//        var a=0;
-//        
-//        while(a<arr.length){
-//            //console.log(arr[a]);
-//            //console.log(arr[a][atributoSeleccionado]);
-//            out += "<option value='"+arr[a][atributoSeleccionado]  +"'"+">"+arr[a][atributoSeleccionado]+"</option>";
-//            a++;
-//        }
-//        
-//        document.getElementById("listaDatos").innerHTML = out;
-//    }
-
-
-
-
-function cargarAtributos(){
-    var xmlhttp = new XMLHttpRequest();
-    var url = "require/cargarAtributos.php";
-    xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            myFunction(xmlhttp.responseText);
-        }
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    function myFunction(response) {
-        var arr = JSON.parse(response);
-        var i;
-        var out = "";
-        //console.log(arr.length);
-        var a=0;
-        while(a<arr.length){
-            //console.log(arr[a]);
-            var arr2 = arr[a];
-            //console.log(" "+arr2[0]); // atributo
-            //console.log(" "+arr2[1]); // descripcion
-            out += "<option value='"+arr2[0]  +"'"+">"+arr2[0]+"</option>";
-            a++;
-        }
-        document.getElementById("listaAtributos").innerHTML = out;
-    }
-}
-
-function cargarValores(){
-    seleccionarAtributo();
-    document.getElementById("listaDatos").innerHTML = "";
-    //console.log("valores de "+atributoSeleccionado);
-    var xmlhttp = new XMLHttpRequest();
-    var url = "require/cargarValores.php?atributoSeleccionado="+atributoSeleccionado;
-    xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            myFunction(xmlhttp.responseText);
-        }
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    function myFunction(response) {
-        var arr = JSON.parse(response);
-        //var id          = arr[0].id;
-        var out = "";
-        //console.log(arr.length);
-        var a=0;
-        while(a<arr.length){
-            //console.log(arr[a]);
-            //console.log(arr[a][atributoSeleccionado]);
-            out += "<option value='"+arr[a][atributoSeleccionado]  +"'"+">"+arr[a][atributoSeleccionado]+"</option>";
-            a++;
-        }
-        document.getElementById("listaDatos").innerHTML = out;
-    }
-}
-
-
-
-function cargarOperadores(){
-    var xmlhttp = new XMLHttpRequest();
-    var url = "require/cargarOperadores.php";
-
-    xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            myFunction(xmlhttp.responseText);
-        }
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-
-    function myFunction(response) {
-        var arr = JSON.parse(response);
-        var i;
-        var out = "";
-        //console.log(arr.length);
-        var a=0;
-        while(a<arr.length){
-            //console.log(arr[a]);
-            var arr2 = arr[a];
-            //console.log(" "+arr2[3]); // atributo 1= nombre, 2= simbolo , 3= descripcion
-            //console.log(" "+arr2[1]); // descripcion
-            out += "<option value='"+arr2[2]  +"'"+">"+arr2[1]+"</option>";
-            a++;
-        }
-        document.getElementById("listaOperadores").innerHTML = out;
-    }
-}
-
 
 
 //guardo el atributo que seleccione
@@ -186,6 +65,109 @@ function consultar(){
     var consulta = document.getElementById("filtroCreado").value;
     console.log("consultar: "+consulta);
 }
+
+
+
+
+
+function cargarAtributos(){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "require/cargarAtributos.php";
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            myFunction(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    function myFunction(response) {
+        var arr = JSON.parse(response);
+        var i;
+        var out = "";
+        //console.log(arr.length);
+        var a=0;
+        while(a<arr.length){
+            //console.log(arr[a]);
+            var arr2 = arr[a];
+            //console.log(" "+arr2[0]); // atributo
+            //console.log(" "+arr2[1]); // descripcion
+            out += "<option value='"+arr2[0]  +"'"+">"+arr2[0]+"</option>";
+            a++;
+        }
+        document.getElementById("listaAtributos").innerHTML = out;
+    }
+}
+
+function cargarValores(){
+
+    if (atributoSeleccionado == undefined){
+        alert("Primero seleccione un atributo");
+    }
+    else{
+
+        document.getElementById("listaDatos").innerHTML = "";
+        //console.log("valores de "+atributoSeleccionado);
+        var xmlhttp = new XMLHttpRequest();
+        var url = "require/cargarValores.php?atributoSeleccionado="+atributoSeleccionado;
+        xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                myFunction(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+        function myFunction(response) {
+            var arr = JSON.parse(response);
+            //var id          = arr[0].id;
+            var out = "";
+            //console.log(arr.length);
+            var a=0;
+            while(a<arr.length){
+                //console.log(arr[a]);
+                //console.log(arr[a][atributoSeleccionado]);
+                out += "<option value='"+arr[a][atributoSeleccionado]  +"'"+">"+arr[a][atributoSeleccionado]+"</option>";
+                a++;
+            }
+            document.getElementById("listaDatos").innerHTML = out;
+        }
+
+    }
+
+
+}
+
+
+
+function cargarOperadores(){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "require/cargarOperadores.php";
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            myFunction(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function myFunction(response) {
+        var arr = JSON.parse(response);
+        var i;
+        var out = "";
+        //console.log(arr.length);
+        var a=0;
+        while(a<arr.length){
+            //console.log(arr[a]);
+            var arr2 = arr[a];
+            //console.log(" "+arr2[3]); // atributo 1= nombre, 2= simbolo , 3= descripcion
+            //console.log(" "+arr2[1]); // descripcion
+            out += "<option value='"+arr2[2]  +"'"+">"+arr2[1]+"</option>";
+            a++;
+        }
+        document.getElementById("listaOperadores").innerHTML = out;
+    }
+}
+
 
 
 
